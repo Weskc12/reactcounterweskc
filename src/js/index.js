82870f261f88af993cae275e56a330d7c6c1a1aa
Wesley Crosslin <136374861+Weskc12@users.noreply.github.com>
2023-09-08 00:14:25 +0000

    counter updated, changed props, added use state and useeffect

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,38 @@
-//import react into the bundle
-import React from "react";
+
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import PropTypes from 'prop-types';
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
-import Home from "./component/home.jsx";
 
 function SimpleCounter(props) {
+    const [counter, setCounter] = useState(0);
+    const four = Math.floor(counter / 1000) ;
+    const three = Math.floor((counter % 1000) / 100);
+    const two = Math.floor((counter % 100)  / 10);
+    const one = counter % 10;
+    console.log(four, three, two, one);
+    useEffect(() =>{
+        const interval = setInterval(() => {
+         setCounter((prevCounter) => prevCounter + 1)   
+        }, 1000 );
+        return () => clearInterval(interval);
+
+    },[] );
+
     return (<div className="bigCounter">
         <div className="calendar">
-            <i class="far fa-clock"></i>
+            <i className="far fa-clock"></i>
         </div>
-        <div className="four">{props.digitFour % 10}</div>
-        <div className="three">{props.digitThree % 10}</div>
-        <div className="two">{props.digitTwo % 10}</div>
-        <div className="one">{props.digitOne % 10}</div>
+        <div className="four">{four % 10}</div>
+        <div className="three">{three % 10}</div>
+        <div className="two">{two % 10}</div>
+        <div className="one">{one % 10}</div>
     </div>);
 }
-SimpleCounter.PropTypes = {
+
+SimpleCounter.propTypes = {
     digitFour: PropTypes.number,
     digitThree: PropTypes.number,
     digitTwo: PropTypes.number,
@@ -28,19 +42,8 @@ SimpleCounter.PropTypes = {
 };
 
 
-let counter = 0;
-setInterval(function () {
-    const four = Math.floor(counter / 1000);
-    const three = Math.floor(counter / 100);
-    const two = Math.floor(counter / 10);
-    const one = Math.floor(counter / 1);
-    console.log(four, three, two, one);
-    counter++;
-}, 1000);
-
-console.log(counter)
 //render your react application
 ReactDOM.render(
-    <SimpleCounter digitOne={counter} digitTwo={counter} digitThree={counter} digitFour={counter} />,
+    <SimpleCounter  />,
     document.querySelector("#app")
 );
